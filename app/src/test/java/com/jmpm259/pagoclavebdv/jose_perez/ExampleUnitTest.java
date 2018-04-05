b9d16776c690765d5e77d147b92e2e5de8b8389c
Jose Perez <jmpm259@gmail.com>
2018-04-04 22:07:18 -0400

    Initial commit

---
+++ app/src/test/java/com/jmpm259/pagoclavebdv/jose_perez/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.jmpm259.pagoclavebdv.jose_perez;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
